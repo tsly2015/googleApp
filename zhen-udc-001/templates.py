5b70a6efa52b90b7f48e954ead01f0e00f150c5b
Zhen <zhenw.wang@gmail.com>
2014-12-03 22:55:42 -0500

    Initial Commit

---
+++ zhen-udc-001/templates.py
@@ -0,0 +1,42 @@
+import os
+import jinja2
+import webapp2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+								autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+	def get(self):
+		items = self.request.get_all("food")
+		self.render("shopping_list.html", items = items)
+
+class MainPage2(Handler):
+	def get(self):
+		n = self.request.get("n")
+		if n:
+			n = int(n)
+		self.render("shopping_list2.html", name1="world",
+						name=self.request.get("name"), n=n)
+
+class FizzBuzzHandler(Handler):
+	def get(self):
+		n = self.request.get('n', 0)
+		n = n and int(n)
+		self.render('fizzbuzz.html', n=n)
+
+app = webapp2.WSGIApplication([('/', MainPage),
+							('/test', MainPage2),
+							('/fizzbuzz', FizzBuzzHandler),
+							], debug=True)
